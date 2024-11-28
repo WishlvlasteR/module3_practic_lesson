b38eedc0acd090a9223f3402e0100727155c5533
Ruslan <wishiviaster99@gmail.com>
2024-11-28 19:33:07 +1200

    26.11.2024

--- module3_practic_lesson_5.py
+++ module3_practic_lesson_5.py
@@ -1,15 +1,16 @@
-    # Определение функции
 def get_multiplied_digits(number):
-    str_number=str(number)                                              # Преобразуем число в строку
-    if len(str_number) > 1:                                             # Если длина строки больше 1, выполняем рекурсию
-        first=int(str_number[0])                                        # Первая цифра
-        return first * get_multiplied_digits(int(str_number[1:]))       # Возвращаем Рекурсивный вызов для оставшейся
-    else:                                                               #                                   части числа
-        return int(str_number[0])                                       # Иначе если длина строки 1,возвращаем саму цифру
-
-    # Определение глобальных переменных с передачей данных аргументов в number
-result_1=get_multiplied_digits(44444)
-result_2=get_multiplied_digits(5555)
+    str_number=str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    elif first != 0:
+        return first
+    else:
+        return 1
+    #else:
+        #return int(str_number[0])
+result_1=get_multiplied_digits(40203)
+result_2=get_multiplied_digits(402030)
     # Вывод результата
 print(result_1)
 print(result_2)
